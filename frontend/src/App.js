d98ba08d1dd92b0b59a43cf0fe7c9af4c66c2696
ahmedbr1 <ahmed.sherif0120055942@gmail.com>
2025-06-28 19:44:28 +0300

    created fontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AudioRecorder from "./AudioRecorder";
+
+function App() {
+  return (
+    <div>
+      <AudioRecorder />
+    </div>
+  );
+}
+
+export default App;
